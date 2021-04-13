d1ca9ececb864afb5a2884069a8c119888823184
lidingyu <lidingyu@sohu.com>
2021-04-13 17:53:29 +0800

    update

---
+++ Assets/Scripts/LongNoteRenderer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [ExecuteInEditMode]
+[RequireComponent(typeof(LineRenderer))]
+public class LongNoteRenderer : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lineRenderer;
+
+    [SerializeField] private GameObject origin;
+
+    [SerializeField] private GameObject destination;
+
+    private void _Awake()
+    {
+        if (lineRenderer == null)
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+
+        if (lineRenderer == null)
+        {
+            return;
+        }
+
+        if (origin != null && destination != null)
+        {
+            lineRenderer.SetPosition(0, origin.transform.position);
+            lineRenderer.SetPosition(1, destination.transform.position);
+        }
+    }
+
+    private void _LateUpdate()
+    {
+        if (lineRenderer == null)
+        {
+            return;
+        }
+
+        if (origin != null && destination != null)
+        {
+            lineRenderer.SetPosition(0, origin.transform.position);
+            lineRenderer.SetPosition(1, destination.transform.position);
+        }
+    }
+
+    [EasyButtons.Button]
+    void UpdateLinePosition()
+    {
+        if (lineRenderer == null)
+        {
+            return;
+        }
+
+        if (origin != null && destination != null)
+        {
+            lineRenderer.SetPosition(0, origin.transform.position);
+            lineRenderer.SetPosition(1, destination.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/NoteBillboardRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBillboardRenderer : MonoBehaviour
+{
+    private Camera _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+        // _camera = Camera.current;
+    }
+
+
+    private void LateUpdate()
+    {
+        if (_camera != null)
+        {
+            transform.LookAt(transform.position + _camera.transform.forward, _camera.transform.up);
+        }
+    }
+}
